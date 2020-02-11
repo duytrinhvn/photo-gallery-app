9e61aca7a7a09da8d12e79c4855ad5e2f005af20
duy.trinh <khac-duy.trinh@mohawkcollege.ca>
2020-02-11 11:56:11 -0500

    update controller, update view

--- app.ctrl.php
+++ app.ctrl.php
@@ -1,9 +1,12 @@
 <?php
-
+// StAuth10065: I Khac Duy Trinh, 000753390 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
 
 // Auto-discover the photos, thumbnails and description files
 $photoData = [];
 $numGal = 0;
+$TPL['currentPhoto'] = -1;
+$TPL['currentDirLen'] = -1;
+$TPL['currentDir'] = -1;
 
 // open the photos folder
 $fp = opendir("photos");
@@ -32,26 +35,38 @@ while( false !== ( $DIR = readdir($fp) ) ) {
   $numGal++;
 }
 
-if(isset($_REQUEST['directory']) && !isset($_REQUEST['id'])){
-  $requestedDir = $_REQUEST['directory'];
-   foreach($photoData as $row ){
-     if($row['directory'] === $requestedDir){
-       $TPL['requestedGal'] = $row;
-       break;
-     }
+if(isset($_REQUEST['act'])){
+  switch($_REQUEST['act']){
+    case 'allphotos':
+      $requestedDir = $_REQUEST['directory'];
+      foreach($photoData as $row ){
+        if($row['directory'] === $requestedDir){
+          $TPL['requestedGal'] = $row;
+          break;
+        }
+      }
+      break;
+    
+    case 'onephoto':
+      $requestedPhoto = $_REQUEST['id'];
+      $requestedDir = $_REQUEST['directory'];
+       foreach($photoData as $row ){
+         if($row['directory'] === $requestedDir){
+            if($requestedPhoto < count($row['photos']) && $requestedPhoto >= 0){
+            $TPL['requestedGal'] = $row;
+            $TPL['requestedPhoto'] = $row['photos'][$requestedPhoto];
+            $TPL['currentPhotoId'] = $_REQUEST['id'] + 1;
+            $TPL['currentDirLen'] = count($row['photos']);
+            $TPL['currentDir'] = $row['directory'];
+           }
+           else {
+            header("Location: app.ctrl.php?directory=" . $requestedDir . "&id=0&act=onephoto");
+           }
+         }
+      }
+      break;
   }
 }
-else if(isset($_REQUEST['directory']) && isset($_REQUEST['id'])) {
-  $requestedPhoto = $_REQUEST['id'];
-  $requestedDir = $_REQUEST['directory'];
-   foreach($photoData as $row ){
-     if($row['directory'] === $requestedDir){
-       $TPL['requestedGal'] = $row;
-       $TPL['requestedPhoto'] = $row['photos'][$requestedPhoto];
-     }
-  }
-}
-
 
 $TPL['results'] = $photoData;
 include 'app.view.php';
--- app.view.php
+++ app.view.php
@@ -1,3 +1,6 @@
+<!-- StAuth10065: I Khac Duy Trinh, 000753390 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else. -->
+
+
 <html>
     <head>
         <title>Photo gallery</title>
@@ -9,13 +12,25 @@
         <div class="container-fluid">
             <h1 class="title"><a href="app.ctrl.php">Photo gallery</a></h1>
             <div class="inner-container">
-
+                <h3 class="description">The Gardens I visited when I went to the Antartctic</h3>
+                <?php 
+                if(isset($TPL['requestedGal']) && !isset($TPL['requestedPhoto'])){ ?>
+                <p class="instruction">Click on a photo to start a slide show!</p>
+                <?php } ?> 
+                <?php if(isset($TPL['requestedGal']) && isset($TPL['requestedPhoto'])){ ?>
+                <div class="photo-nav">
+                    <a href="app.ctrl.php?act=onephoto&directory=<?php echo $TPL['currentDir']; ?>&id=<?php echo (int) $TPL['currentPhotoId'] - 2 ; ?>" class="btn btn-primary">Prev</a>
+                    <a href="app.ctrl.php?act=onephoto&directory=<?php echo $TPL['currentDir']; ?>&id=<?php echo $TPL['currentPhotoId'] ; ?>" class="btn btn-primary">Next</a>
+                    <p class="photo-counter">(<?php echo $TPL['currentPhotoId']; ?>/<?php echo $TPL['currentDirLen']; ?>)</p>
+                    <a href="app.ctrl.php?directory=<?=$TPL['requestedGal']['directory']?>&act=allphotos" class="btn btn-primary">Show all photos</a>
+                </div>
+                <?php  } ?>
                 <div class="row">
                     <?php 
                     if(!isset($TPL['requestedGal'])){
                         foreach ($TPL['results'] as $row) { ?>
                             <div class="card col-sm" style="padding-right: 0; padding-left: 0">
-                                <a href="app.ctrl.php?directory=<?=$row['directory']?>">
+                                <a href="app.ctrl.php?directory=<?=$row['directory']?>&act=allphotos">
                                     <img src="./photos/<?php echo $row['directory'] . '/' . $row['photos'][0]; ?>" class="card-img-top img" alt="..." />
                                     <div class="card-body">
                                         <h5 class="card-title"><?php echo $row['description']; ?></h5>
@@ -30,7 +45,7 @@
                         for($i = 0; $i < count($TPL['requestedGal']['photos']); $i++){
                             ?>
                             <div class="col-lg-3 col-md-4 col-sm-6">
-                                <a href="app.ctrl.php?directory=<?=$TPL['requestedGal']['directory']; ?>&id=<?= $i; ?>">
+                                <a href="app.ctrl.php?directory=<?=$TPL['requestedGal']['directory']; ?>&id=<?= $i; ?>&act=onephoto">
                                     <img src="./photos/<?php echo $TPL['requestedGal']['directory'] . '/' . $TPL['requestedGal']['photos'][$i]; ?>" class="img-thumbnail" />
                                 </a>
                             </div>
